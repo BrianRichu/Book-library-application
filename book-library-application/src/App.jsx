de8d9c66680928f79830b280ce88c6abaec32a14
Brian Richu <richubrian427@gmail.com>
2025-10-06 11:41:29 +0300

    test: verify Google Books api connection

--- book-library-application/src/App.jsx
+++ book-library-application/src/App.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-
+import { useEffect } from "react";
+import { fetchBooks } from "./services/bookService";
 
 function App() {
+  useEffect(() => {
+    fetchBooks("harry potter").then(console.log);
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-600">Book Library App</h1>
     </div>
   );
 }
+
 export default App;
