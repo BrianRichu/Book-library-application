dbf4421b47006356272d94513ba788b23dd04e47
Brian Richu <richubrian427@gmail.com>
2025-10-06 11:37:33 +0300

    configured tailwind into application

--- book-library-application/src/App.jsx
+++ book-library-application/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+import React from "react";
 
 
 function App() {
   return (
-    <>
-      <p>Book Application Library</p>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Book Library App</h1>
+    </div>
+  );
 }
-
-export default App
+export default App;
